97a18bd259651b18c8c3a19f8e630fd3245a0094
PratikGkd <pratikgkd@yahoo.in>
2024-09-15 00:26:56 +0530

    exercise 1 done

---
+++ src/main/java/com/pratikgkd/reactive_programming_basics/ReactiveProgrammingBasicsApplication.java
@@ -0,0 +1,13 @@
+package com.pratikgkd.reactive_programming_basics;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReactiveProgrammingBasicsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReactiveProgrammingBasicsApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/pratikgkd/reactive_programming_basics/exercise_files/Exercise1.java
@@ -0,0 +1,50 @@
+package com.pratikgkd.reactive_programming_basics.exercise_files;
+
+import com.pratikgkd.reactive_programming_basics.helpers.StreamSources;
+
+public class Exercise1 {
+
+  public static void main(String[] args) {
+
+    // Print all numbers in the intNumbersStream stream
+    StreamSources.intNumbersStream()
+        .forEach(num -> System.out.println(num));
+
+    // Print numbers from intNumbersStream that are less than 5
+    StreamSources.intNumbersStream()
+        .filter(num -> num < 5)
+        .forEach(num -> System.out.println(num));
+
+    // Print the second and third numbers in intNumbersStream that's greater than 5
+    StreamSources.intNumbersStream()
+        .filter(num -> num > 5)
+        .skip(1)
+        .limit(2)
+        .forEach(num -> System.out.println(num));
+
+    //  Print the first number in intNumbersStream that's greater than 5.
+    //  If nothing is found, print -1
+    Integer greateThenFive = StreamSources.intNumbersStream()
+        .filter(num -> num > 5)
+        .findFirst()
+        .orElse(-1);
+    System.out.println("First greater than 5 number is " + greateThenFive);
+
+    // Print first names of all users in userStream
+    StreamSources.userStream()
+        .forEach(user -> System.out.println(user.getFirstName()));
+
+    // Print first names in userStream for users that have IDs from number stream
+    StreamSources.userStream()
+        .filter(user -> StreamSources.intNumbersStream().anyMatch(id -> id == user.getId()))
+        .forEach(System.out::println);
+
+    //with flatMap
+    System.out.println("\nWith flatmap::::");
+    StreamSources.intNumbersStream()
+        .flatMap(id -> StreamSources.userStream().filter(user -> user.getId() == id))
+        .map(user -> user.getFirstName())
+        .forEach(System.out::println);
+  }
+
+}
