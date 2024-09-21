d88ba6291720bfb5f3dd7a40999b5929605bb6af
PratikGkd <pratikgkd@yahoo.in>
2024-09-21 17:40:46 +0530

    exercise 4 done

---
+++ src/main/java/com/pratikgkd/reactive_programming_basics/exercise_files/IntroToMono.java
@@ -0,0 +1,28 @@
+package com.pratikgkd.reactive_programming_basics.exercise_files;
+
+import com.pratikgkd.reactive_programming_basics.helpers.ReactiveSources;
+import java.io.IOException;
+import java.util.Optional;
+
+/**
+ * Exervice 4: Intro to Mono
+ * https://www.youtube.com/watch?v=2C39c4iy0wI&list=PLqq-6Pq4lTTYPR2oH7kgElMYZhJd4vOGI&index=16
+ */
+public class IntroToMono {
+
+  public static void main(String[] args) throws IOException {
+
+    // Use ReactiveSources.intNumberMono()
+
+    // Print the value from intNumberMono when it emits
+    ReactiveSources.intNumberMono().subscribe(num -> System.out.println(num));
+
+    // Get the value from the Mono into an integer variable
+    Integer num = ReactiveSources.intNumberMono().block();
+    Optional<User> userFoo = ReactiveSources.userMono().blockOptional();
+
+    System.out.println("Press a key to end");
+    System.in.read();
+  }
+
+}
